5d9045fe76c853d9660566c0ca6986c80cb1025c
sujeetraja220 <vipsujeetkumar>
2022-12-09 23:26:44 +0530

    Imagecard updated

--- src/Component/ImageCards.jsx
+++ src/Component/ImageCards.jsx
@@ -6,38 +6,7 @@ import poojs from "./imag/pooja.jpg"
 function ImageCards() {
   return (
    <>
-    <Card style={{ width: '15rem', display:'inline'}}>
-      <Card.Img variant="top" src={poojs} />
-      <Card.Body>
-        <Card.Title>Sunney Leon</Card.Title>
-        <Card.Text>Sunney leon hot photo </Card.Text>
-        <Button variant="primary">Downlod</Button>
-      </Card.Body>
-    </Card>
-    <Card style={{ width: '15rem', display:'inline'}}>
-      <Card.Img variant="top" src={poojs} />
-      <Card.Body>
-        <Card.Title>Sunney Leon</Card.Title>
-        <Card.Text>Sunney leon hot photo </Card.Text>
-        <Button variant="primary">Downlod</Button>
-      </Card.Body>
-    </Card>
-    <Card style={{ width: '15rem', display:'inline'}}>
-      <Card.Img variant="top" src={poojs} />
-      <Card.Body>
-        <Card.Title>Sunney Leon</Card.Title>
-        <Card.Text>Sunney leon hot photo </Card.Text>
-        <Button variant="primary">Downlod</Button>
-      </Card.Body>
-    </Card>
-    <Card style={{ width: '15rem', display:'inline'}}>
-      <Card.Img variant="top" src={poojs} />
-      <Card.Body>
-        <Card.Title>Sunney Leon</Card.Title>
-        <Card.Text>Sunney leon hot photo </Card.Text>
-        <Button variant="primary">Downlod</Button>
-      </Card.Body>
-    </Card>
+    <h1>Image Card</h1>
     </>
   );
 }
