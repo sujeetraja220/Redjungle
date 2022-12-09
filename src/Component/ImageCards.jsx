5a2b9bf8f1b6e86162f85409e05ab296bdf88c48
sujeetraja220 <vipsujeetkumar>
2022-12-09 23:43:21 +0530

    image card change

--- src/Component/ImageCards.jsx
+++ src/Component/ImageCards.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import poojs from "./imag/pooja.jpg"
 
 function ImageCards() {
   return (
